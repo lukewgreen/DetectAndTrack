6871fff49a1180a3aaf472c965e364bc13c62bc0
Ubuntu <ubuntu@ip-172-31-72-241.ec2.internal>
2018-09-15 14:04:31 +0000

    .

--- create_container.sh
+++ create_container.sh
@@ -1,1 +1,1 @@
-sudo nvidia-docker run -it -v /home/ubuntu/docker_volumes/data2:/data2 $0 bash
+sudo nvidia-docker run -it -v /home/ubuntu/docker_volumes/data2:/data2 $1 bash
--- docker/create_container.sh
+++
@@ -1,1 +0,0 @@
-sudo nvidia-docker run -it -v /home/ubuntu/docker_volumes/data2:/data2 $0 bash
